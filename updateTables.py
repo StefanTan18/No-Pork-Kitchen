af8b094737fda89fe47933302ca4b76165e28543
Ahmed Imran <aimran1239@gmail.com>
2022-05-13 02:18:47 -0400

    Finished Orders/OrderItems Functions

--- updateTables.py
+++ updateTables.py
@@ -1,4 +1,6 @@
 from tabnanny import check
+from unicodedata import decimal
+from venv import create
 from webbrowser import get
 import mysql.connector
 from mysqlx import IntegrityError
@@ -280,11 +282,17 @@ def getAllOfType(type):
     result = mycursor.fetchall()
     print (result)
     return result
+#Get item by ID
+def getItem(itemID):
+    mycursor.execute("SELECT * FROM Food WHERE item_no = '%s'" % itemID)
+    result = mycursor.fetchall()
+    print (result)
+    return result
 #Add item (id,price,name,description,type,image)
-def addItem(id,price,name,description,type,image):
+def addItem(id,price,name,description,type,chef,image):
     try:
-        sql = "INSERT INTO Food (item_no,price,name,description,itemtype,no_sold,image) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        val = (id,price,name,description,type,0,image)
+        sql = "INSERT INTO Food (item_no,price,name,description,itemtype,no_sold,chefID,image) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (id,price,name,description,type,0,chef,image)
         mycursor.execute(sql, val)
         mydb.commit()
         print("Item added to menu.")
@@ -292,8 +300,9 @@ def addItem(id,price,name,description,type,image):
     except IntegrityError:
         print("Item already exists.")
         return False
-    except:
-        print("An exception occurred.")
+    except Exception as e:
+        print("An exception occurred. Failed to add item.")
+        print(e)
         return False
 #Update item description(id, description)
 def updateItemDescription(id, description):
@@ -333,15 +342,124 @@ def deleteItem(id):
 #############################################################################################
 
 #Get all orders
+def getAllOrders():
+    mycursor.execute("SELECT * FROM Orders")
+    result = mycursor.fetchall()
+    print (result)
+    return result
 #Get all incomplete orders
+def getAllIncompleteOrders():
+    mycursor.execute("SELECT * FROM Orders WHERE status != 'done'")
+    result = mycursor.fetchall()
+    print (result)
+    return result
 #Get all orders for CustomerID
+def getAllOrdersForCust(id):
+    mycursor.execute("SELECT * FROM Orders WHERE customerID = "+str(id))
+    result = mycursor.fetchall()
+    print (result)
+    return result
 #Get all orders for DeliveryID
-#Get all orders for ChefID
-#Create new order (orderid,customerid,arrayof[chefID,item,price,quantity])
-#Assign delivery people(userid)
-#Update order status
-#Reviews (delivery_rating,delivery_review,arrayof[item_rating,item_reviews])
+def getAllOrdersForDelivery(id):
+    mycursor.execute("SELECT * FROM Orders WHERE DeliveryID = "+str(id))
+    result = mycursor.fetchall()
+    print (result)
+    return result
+#Get all items from order
+def getItemsFromOrder(orderid):
+    mycursor.execute("SELECT * FROM OrderItems WHERE order_no = "+str(orderid))
+    result = mycursor.fetchall()
+    print (result)
+    return result
+#Create new order (orderid,customerid,arrayof[(item,quantity)])
+def createOrder(orderid,customerid,items):
+    try:
+        sql = "INSERT INTO Orders (order_no,customerID,total_price,status) VALUES (%s, %s, %s, %s)"
+        val = (orderid,customerid,0.00,"Order Received")
+        mycursor.execute(sql, val)
+        price = 0.00
+        for i in items:
+            price += float(addItemToOrder(orderid,i))
+        sql = "UPDATE Orders SET total_price = %s WHERE order_no = %s"
+        val = (price,orderid)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print("Order created successfully")
+        return 1
+    except Exception as e:
+        print("Order failed. Please try again.")
+        print(e)
+        return 0
+#Helper Function additems to add to orderitems table item = (item_no,quantity)
+def addItemToOrder(order_no,item):
+    try:
+        mycursor.execute("SELECT * from Food where item_no = '%s'" % item[0])
+        result = mycursor.fetchone()
+        t_price = result[1] * item[1]
+        sql = "INSERT INTO OrderItems (order_no,item_no,price,quantity) VALUES (%s, %s, %s, %s)"
+        val = (order_no,item[0],t_price,item[1])
+        mycursor.execute(sql, val)
+        mydb.commit()
+        return t_price; 
+    except Exception as e:
+        print("Unable to add Item.")
+        print(e)
+        return 0
 #Delete order(id)
+def deleteOrder(orderID):
+    try: 
+        sql = "DELETE FROM Orders WHERE order_no = %s" % orderID
+        mycursor.execute(sql)
+        mydb.commit()
+    except Exception as e:
+        print("Failed to Delete Order")
+        print(e)
+#Assign delivery people(userid)
+def assignDelivery(orderID,deliveryID):
+    try:
+        sql = "UPDATE Orders SET deliveryID = %s WHERE order_no = %s"
+        val = (deliveryID,orderID)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    except Exception as e:
+        print ("Failed to add delivery driver")
+        print(e)
+#Update order status
+def updateOrderStatus(orderID,status):
+    try:
+        sql = "UPDATE Orders SET status = %s WHERE order_no = %s"
+        val = (status,orderID)
+        mycursor.execute(sql, val)
+        mydb.commit()
+    except Exception as e:
+        print ("Failed to update status")
+        print(e)
+#Reviews (delivery_rating,delivery_review)
+def addDeliveryReview(orderID,delivery_rating,delivery_review):
+    try:
+        if (delivery_rating is not None):
+            sql = "UPDATE Orders SET delivery_rating = %s, delivery_review = %s WHERE order_no = %s"
+            val = (delivery_rating,delivery_review,orderID)
+            mycursor.execute(sql, val)
+        mydb.commit()
+    except Exception as e:
+        print ("Failed to update status")
+        print(e)
+#Reviews (arrayof[(item_no,item_rating,item_reveiew)])
+def addItemsReview(orderID,item_reviews):
+    try:
+        for i in item_reviews:
+            itemID = i[0]
+            itemRating = i[1]
+            itemReview = i[2]
+            sql = "UPDATE OrderItems SET item_rating = %s, item_review = %s WHERE order_no = %s AND item_no = %s"
+            val = (itemRating,itemReview,orderID,itemID)
+            mycursor.execute(sql, val)
+        mydb.commit()
+    except Exception as e:
+        print ("Failed to add item reviews")
+        print(e)
+
 
 #File complaint/compliment (entry_no,fault_id,reviewer_id,type(+ or -),critique,status)
 #Update complaint/compliment(status)
