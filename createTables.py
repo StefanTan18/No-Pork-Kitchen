001f9e58fe12117090b86212da9e5169ed8d592a
Stefan Tan <stefantan39@yahoo.com>
2022-05-16 12:21:17 -0400

    undo changes

--- createTables.py
+++ createTables.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "5326atAT!", # Change accordingly
+    password = "Youshallnotpass!", # Change accordingly
     database = "NoPorkKitchenDB"
 )
 
